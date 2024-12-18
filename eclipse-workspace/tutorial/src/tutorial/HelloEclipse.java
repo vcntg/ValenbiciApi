9d690a1798c9752755bc3b73c3043a1e84f95107
Vicente Gil DAW <vicgilsan4@alu.edu.gva.es>
2024-12-18 10:28:41 +0100

    Primer commit

---
+++ eclipse-workspace/tutorial/src/tutorial/HelloEclipse.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class HelloEclipse {
+
+	public HelloEclipse() {
+		// TODO Esbozo de constructor generado automáticamente
+	}
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+
+	}
+
+}
